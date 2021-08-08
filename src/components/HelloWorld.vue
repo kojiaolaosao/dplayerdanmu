33c335f39fe7c5eb7f5a10d3213e1e4a5a21f2e4
laosao <kojiaolaosao@gmail.com>
2021-08-09 07:02:00 +0800

    dplayer实现弹幕功能，高德定位

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,136 @@
 <template>
-  <div id="dp" style="width: 885px;height: 500px"></div>
+  <div>
+    <div id="dp" style="width: 885px;height: 500px"></div>
+    <!-- 地图容器 -->
+    <div id="map_container" style="width: 800px;height: 700px;float: right;margin-top:-500px;right: 50px">
+      <!-- 地图 -->
+      <el-amap :center="center" :zoom="zoom" class="amap_demo" :plugin="plugin">
+        <!-- 地图标记 -->
+        <el-amap-marker v-for="(marker,index) in markers" :position="marker.position" :key="index"></el-amap-marker>
+      </el-amap>
+    </div>
+  </div>
+
+
 </template>
 
 <script>
 // import DPlayer from '../../node_modules/dplayer'
 import DPlayer from 'dplayer'
+import AMap from "postcss";
 // const dp = new DPlayer(options);
 
 
 export default {
   data() {
+    const that = this;
     return {
       dp: null,
+      zoom: 16,
+      // 默认中心点
+      // center: [121.59996, 31.197646],
+      // 标记点
+      markers: [
+        // 标记点位置
+        // {position: [121.59996, 31.197646],}
+      ],
+      // 当前地图的插件
+      plugin: [{
+        enableHighAccuracy: true,//是否使用高精度定位，默认:true
+        timeout: 10000,          //超过10秒后停止定位，默认：无穷大
+        maximumAge: 0,           //定位结果缓存0毫秒，默认：0
+        convert: true,           //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
+        showButton: true,        //显示定位按钮，默认：true
+        buttonPosition: 'RB',    //定位按钮停靠位置，默认：'LB'，左下角
+        showMarker: false,        //定位成功后在定位到的位置显示点标记，默认：true
+        showCircle: true,        //定位成功后用圆圈表示定位精度范围，默认：true
+        panToLocation: true,     //定位成功后将定位到的位置作为地图中心点，默认：true
+        zoomToAccuracy: true,//定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：f
+        extensions: 'all',
+        pName: 'Geolocation',
+        events: {
+          init(o) {
+            // o 是高德地图定位插件实例
+            o.getCurrentPosition((status, result) => {
+              console.log(result)
+              if (result && result.position) {
+                // 将当前经纬度给中心点
+                that.center = [result.position.lng, result.position.lat];
+                // 将当前经纬度给标记点
+                that.markers[0].position = that.center;
+                that.loaded = true;
+                that.$nextTick();
+              }
+            });
+          }
+        }
+      }],
+
     }
   },
   methods: {
     onplay() {
       this.onplay();
-    }
+    },
+    // methods部分
+    getLocation() {
+      const self = this;
+      AMap.plugin('AMap.Geolocation', function () {
+        var geolocation = new AMap.Geolocation({
+          // 是否使用高精度定位，默认：true
+          enableHighAccuracy: true,
+          // 设置定位超时时间，默认：无穷大
+          timeout: 10000,
+        })
+
+        geolocation.getCurrentPosition()
+        AMap.event.addListener(geolocation, 'complete', onComplete);
+        AMap.event.addListener(geolocation, 'error', onError);
+
+        function onComplete(data) {
+          // data是具体的定位信息
+          console.log('定位成功信息：', data);
+        }
+
+        function onError(data) {
+          // 定位出错
+          console.log('定位失败错误：', data);
+          // 调用ip定位
+          self.getLngLatLocation();
+        }
+      })
+    },
+    getLngLatLocation() {
+      AMap.plugin('AMap.CitySearch', function () {
+        var citySearch = new AMap.CitySearch();
+        citySearch.getLocalCity(function (status, result) {
+          if (status === 'complete' && result.info === 'OK') {
+            // 查询成功，result即为当前所在城市信息
+            console.log('通过ip获取当前城市：', result)
+            //逆向地理编码
+            AMap.plugin('AMap.Geocoder', function () {
+              var geocoder = new AMap.Geocoder({
+                // city 指定进行编码查询的城市，支持传入城市名、adcode 和 citycode
+                city: result.adcode
+              })
+
+              var lnglat = result.rectangle.split(';')[0].split(',');
+              geocoder.getAddress(lnglat, function (status, data) {
+                if (status === 'complete' && data.info === 'OK') {
+                  // result为对应的地理位置详细信息
+                  console.log(data)
+                }
+              })
+            })
+          }
+        })
+      })
+    },
+  },
+// created 中调用
+  created() {
+    // 此处为调用精确定位之后，调取ip定位，可根据实际情况改写
+    this.getLocation();
   },
   mounted() {
     // eslint-disable-next-line no-unused-vars
@@ -130,5 +243,16 @@ li {
 
 a {
   color: #42b983;
+}
+
+/*1.amap_demo必须有高度：以下是css代码*/
+#map_container {
+  width: 100%;
+  height: 210px;
+}
+
+.amap_demo {
+  width: 100%;
+  height: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueAMap from 'vue-amap'
+
+
+Vue.use(VueAMap)
+VueAMap.initAMapApiLoader({
+  // key: 'dd4cc5f831b67cde767de9a65f983650',
+  key: '4c6c906feaedd18afd1a5f12f9fbbc26',
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar',
+    'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor','AMap.Geolocation'],
+  // 默认高德 sdk 版本为 1.4.4
+  v: '1.4.4',
+  uiVersion:'1.0.11'
+})
+
 
 Vue.config.productionTip = false
 
